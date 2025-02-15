14e44be4d603b73d816e97531659c4ac8c2139c6
GigaZelensky <85454363+GigaZelensky@users.noreply.github.com>
2025-02-15 19:05:16 +0000

    Update CommandRegister.java

--- src/main/java/ac/grim/grimac/manager/init/start/CommandRegister.java
+++ src/main/java/ac/grim/grimac/manager/init/start/CommandRegister.java
@@ -4,6 +4,14 @@ import ac.grim.grimac.GrimAPI;
 import ac.grim.grimac.commands.*;
 import ac.grim.grimac.manager.init.Initable;
 import co.aikar.commands.PaperCommandManager;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import ac.grim.grimac.api.AbstractCheck;
+import ac.grim.grimac.checks.Check;
+import ac.grim.grimac.player.GrimPlayer;
+
+import java.util.Set;
+import java.util.TreeSet;
 
 public class CommandRegister implements Initable {
     @Override
@@ -27,7 +35,49 @@ public class CommandRegister implements Initable {
         commandManager.registerCommand(new GrimDump());
         commandManager.registerCommand(new GrimBrands());
         commandManager.registerCommand(new GrimHistory());
+        commandManager.registerCommand(new GrimResetVl());
 
         commandManager.getCommandCompletions().registerCompletion("stopspectating", GrimStopSpectating.completionHandler);
+
+        commandManager.getCommandCompletions().registerCompletion("checks", context -> {
+            Set<String> suggestions = new TreeSet<>();
+            suggestions.add("all");
+            if (context.getSender() instanceof Player) {
+                Player player = (Player) context.getSender();
+                GrimPlayer gp = GrimAPI.INSTANCE.getPlayerDataManager().getPlayer(player);
+                if (gp != null) {
+                    for (AbstractCheck absCheck : gp.checkManager.allChecks.values()) {
+                        if (absCheck instanceof Check) {
+                            Check c = (Check) absCheck;
+                            if (c.getCheckName() != null && !c.getCheckName().isEmpty()) {
+                                suggestions.add(c.getCheckName());
+                            }
+                            if (c.getAlternativeName() != null && !c.getAlternativeName().isEmpty()) {
+                                suggestions.add(c.getAlternativeName());
+                            }
+                        }
+                    }
+                }
+            } else {
+                for (GrimPlayer gp : GrimAPI.INSTANCE.getPlayerDataManager().getEntries()) {
+                    for (AbstractCheck absCheck : gp.checkManager.allChecks.values()) {
+                        if (absCheck instanceof Check) {
+                            Check c = (Check) absCheck;
+                            if (c.getCheckName() != null && !c.getCheckName().isEmpty()) {
+                                suggestions.add(c.getCheckName());
+                            }
+                            if (c.getAlternativeName() != null && !c.getAlternativeName().isEmpty()) {
+                                suggestions.add(c.getAlternativeName());
+                            }
+                        }
+                    }
+                }
+            }
+            return suggestions;
+        });
+
+        if (GrimAPI.INSTANCE.getConfigManager().getConfig().getBooleanElse("check-for-updates", true)) {
+            GrimVersion.checkForUpdatesAsync(Bukkit.getConsoleSender());
+        }
     }
 }
