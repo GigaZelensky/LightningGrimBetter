13dbc54e0f550234726c4d3892d124af342d5b8b
GigaZelensky <tomasvenrura13@gmail.com>
2025-05-21 16:16:48 +0100

    Update FastPlace.java

--- common/src/main/java/ac/grim/grimac/checks/impl/scaffolding/FastPlace.java
+++ common/src/main/java/ac/grim/grimac/checks/impl/scaffolding/FastPlace.java
@@ -142,7 +142,7 @@ public class FastPlace extends Check implements PacketCheck {
                 if (avgNs <= P1_NS) {
                     baseLimit = 0.45D - 0.10D * (avgNs / (double) P1_NS);
                 } else {
-                    baseLimit = 0.35D - 0.20D *
+                    baseLimit = 0.35D - 0.25D *
                                 ((avgNs - P1_NS) / (double) (MAX_FLAG_AVG_NS - P1_NS));
                 }
                 baseLimit = Math.max(baseLimit, 0.15D);
